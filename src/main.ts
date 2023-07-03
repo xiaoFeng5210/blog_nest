7ef63417a1555001b98e4b5f36a2d022a250afd1
Eadweard <804875728@qq.com>
2023-07-03 17:29:41 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import {
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { generateDocument } from './doc';
 
 declare const module: any;
 
@@ -26,6 +27,7 @@ async function bootstrap() {
   });
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
+  generateDocument(app)
   await app.listen(5000);
 }
 bootstrap();
